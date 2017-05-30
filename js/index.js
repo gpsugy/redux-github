f9818e0baf1e2035ae4431ffe48353a55917b327
Robby Kim <robby@thenewfoundry.com>
2017-05-30 16:26:55 -0400

    Implement reducer

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
 require('babel-polyfill');
+
+require('./actions/index');
+require('./reducers/index');
---
+++ js/reducers/index.js
@@ -0,0 +1,30 @@
+import { ADD_REPOSITORY, RATE_REPOSITORY } from '../actions'
+
+export function respositoryReducer(state = [], action) {
+	switch (action.type) {
+		case ADD_REPOSITORY:
+			return ...state, {
+				name: action.repo,
+				rating: null
+			}
+		case RATE_REPOSITORY: {
+			let index = -1;
+			for (let i=0; i<state.length; i++) {
+				if (state[i].name == action.repo) {
+					index = i;
+					break;
+				}
+			}
+
+			if (index === -1) {
+				throw new Error('Could not find repository to rate');
+			}
+
+			let before = state.slice(0, index);
+			let after = state.slice(index + 1);
+			return before.concat(...state[index], {rating: action.rating)}, after);
+		}
+		default:
+			return state;
+	}
+}
