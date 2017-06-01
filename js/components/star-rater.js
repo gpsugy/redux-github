6c63dc1e97f35159e3d00a9d8bfef9f694861eb9
Robby Kim <robby@thenewfoundry.com>
2017-06-01 10:11:21 -0400

    Show 5 stars at all times

--- js/components/star-rater.js
+++ js/components/star-rater.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function renderStars(rating) {
 	let stars = [];
-	for (let i=0; i<rating; i++) {
-		stars.push(<i key={i} className="fa fa-star"></i>);
+	for (let i=0; i<5; i++) {
+		(i < rating - 1) ? stars.push(<i key={i} className="fa fa-star"></i>)
+					: stars.push(<i key={i} className="fa fa-star-o"></i>);
 	}
 	return stars;
 }
