7fe308b1e5cfd98fbffa2c0d8ade7197313c2865
Robby Kim <robby@thenewfoundry.com>
2017-06-01 14:37:46 -0400

    Implement working ContainerStarRater

--- js/components/star-rater.js
+++ js/components/star-rater.js
@@ -3,8 +3,8 @@ import React from 'react';
 function renderStars(rating, handleClick) {
 	let stars = [];
 	for (let i=0; i<5; i++) {
-		(i < rating - 1) ? stars.push(<i key={i} className="fa fa-star"></i>)
-					: stars.push(<i key={i} className="fa fa-star-o"></i>);
+		(i < rating) ? stars.push(<i key={i} className="fa fa-star" onClick={() => handleClick(i+1)}></i>)
+					: stars.push(<i key={i} className="fa fa-star-o" onClick={() => handleClick(i+1)}></i>);
 	}
 	return stars;
 }
--- js/containers/container-star-rater.js
+++ js/containers/container-star-rater.js
@@ -5,22 +5,23 @@ import StarRater from '../components/star-rater';
 
 const mapStateToProps = (state, ownProps) => {
 	let index = -1;
-	for (let i=0; i<state.length; i++) {
-		if (state[i].name == ownProps.name) {
+	for (let i=0; i<state.repoList.length; i++) {
+		if (state.repoList[i].name == ownProps.name) {
 			index = i;
 			break;
 		}
 	}
-
+	// console.log(index !== -1 ? state.repoList[index].rating : null);
 	return {
-		rating: index !== -1 ? state[index].rating : null
+		rating: index !== -1 ? state.repoList[index].rating : null
 	}
 }
 
-const mapDispatchToProps = (dispatch, onwProps) => {
+const mapDispatchToProps = (dispatch, ownProps) => {
 	return {
-		handleClick: () => {
-			dispatch(rateRepository(ownProps.name, ownProps.rating))
+		handleClick: (rating) => {
+			// console.log('handleClick clicked with rating: ' + rating)
+			dispatch(rateRepository(ownProps.name, rating))
 		}
 	}
 }
