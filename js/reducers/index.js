5df3b64086138752517b59b8d46b7f7b1d3cb6e4
Robby Kim <robby@thenewfoundry.com>
2017-06-02 11:43:44 -0400

    Create function to find index in reducer

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,5 +1,21 @@
 import { ADD_REPOSITORY, RATE_REPOSITORY } from '../actions'
 
+function findIndex(state, name) {
+	let index = -1;
+	for (let i=0; i<state.length; i++) {
+		if (state[i].name == name) {
+			index = i;
+			break;
+		}
+	}
+
+	if (index === -1) {
+		throw new Error('Could not find repository to rate');
+	}
+
+	return index;
+}
+
 function repositoryReducer(state = [], action) {
 	switch (action.type) {
 		case ADD_REPOSITORY:
@@ -11,17 +27,7 @@ function repositoryReducer(state = [], action) {
 				}
 			]
 		case RATE_REPOSITORY:
-			let index = -1;
-			for (let i=0; i<state.length; i++) {
-				if (state[i].name == action.repo) {
-					index = i;
-					break;
-				}
-			}
-
-			if (index === -1) {
-				throw new Error('Could not find repository to rate');
-			}
+			let index = findIndex(state, action.repo);
 
 			let before = state.slice(0, index);
 			let after = state.slice(index + 1);
