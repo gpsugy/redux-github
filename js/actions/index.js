83b625da72ab77fb53494e1aed437919f7191d8a
Robby Kim <robby@thenewfoundry.com>
2017-06-02 11:32:32 -0400

    Implement new async actions

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,6 +1,8 @@
 // action types
 export const ADD_REPOSITORY = 'ADD_REPOSITORY';
 export const RATE_REPOSITORY = 'RATE_REPOSITORY';
+export const FETCH_DESCRIPTION_SUCCESS = 'FETCH_DESCRIPTION_SUCCESS';
+export const FETCH_DESCRIPTION_ERROR = 'FETCH_DESCRIPTION_ERROR';
 
 // action creators
 export function addRepository(repo) {
@@ -18,7 +20,21 @@ export function rateRepository(repo, rating) {
 	}
 }
 
+export function fetchDescriptionSuccess(repo, description) {
+	return {
+		type: FETCH_DESCRIPTION_SUCCESS,
+		repo: repo,
+		description: description
+	}
+}
 
+export function fetchDescriptionError(repo, error) {
+	return {
+		type: FETCH_DESCRIPTION_ERROR,
+		repo: repo,
+		error: error
+	}
+}
 
 // Store State Model - an array of repo objects
 // {
